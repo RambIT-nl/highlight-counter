7b4a10495755a15efb7c81e4539a64d326efce55
Ramon Bakker <ramonbakker@rambit.nl>
2019-08-19 19:32:38 +0200

    Disable case sensitivity by default
    This disables case sensitivity by default and adds a setting to be able to control case sensitivity.

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,14 @@ import { window, workspace, StatusBarAlignment, StatusBarItem } from 'vscode';
 
 let alignment: number;
 let priority: number;
+let caseSensitive: boolean;
 let msg: StatusBarItem;
 
 workspace.onDidChangeConfiguration((event) => {
     if (
         event.affectsConfiguration('highlight-counter.alignment') ||
-        event.affectsConfiguration('highlight-counter.priority')
+        event.affectsConfiguration('highlight-counter.priority') ||
+        event.affectsConfiguration('highlight-counter.caseSensitive')
     ) {
         // Reactivate the extension to apply the new configuration
         deactivate();
@@ -18,7 +20,14 @@ workspace.onDidChangeConfiguration((event) => {
 export function activate() {
     alignment = getAlignmentValue();
     priority = getPriorityValue();
+    caseSensitive = getCaseSensitiveValue();
     msg = window.createStatusBarItem(alignment, priority);
+
+    let matchFlags = 'gi';
+
+    if (caseSensitive) {
+        matchFlags = 'g';
+    }
 
     window.onDidChangeTextEditorSelection(() => {
         let editor = window.activeTextEditor;
@@ -28,12 +37,13 @@ export function activate() {
 
             if (text.length >= 1 && text.indexOf('\n') === -1) {
                 text = text.replace(/\\/g, '\\\\');
+
                 let matches = editor.document
                     .getText()
-                    .match(new RegExp(text, 'g'));
+                    .match(new RegExp(text, matchFlags));
 
                 if (matches !== null && matches.length >= 1) {
-                    msg.text = 'Highlighted: ' + matches.length;
+                    msg.text = 'Highlighted hoi: ' + matches.length;
                     msg.show();
                 } else {
                     msg.hide();
@@ -75,4 +85,15 @@ function getPriorityValue(): number {
         .get('priority');
 
     return Number(priority);
+}
+
+/**
+ * Get the configured case sensitive value.
+ */
+function getCaseSensitiveValue(): boolean {
+    let caseSensitive: boolean | undefined = workspace
+        .getConfiguration('highlight-counter')
+        .get('caseSensitive');
+
+    return caseSensitive ? caseSensitive : false;
 }
